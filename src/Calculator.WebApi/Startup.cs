d83eea6b1904d533e6990e1d8857d797e6254b5f
DESKTOP-K9B8QSD\gilad <giladsegal123@gmail.com>
2018-04-19 10:12:35 +0300

    change base url

--- src/Calculator.WebApi/Startup.cs
+++ src/Calculator.WebApi/Startup.cs
@@ -18,18 +18,22 @@ namespace Calculator.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             // Register the Swagger generator, defining one or more Swagger documents  
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+                
+                c.SwaggerDoc("v1", new Info {  Title = "My API", Version = "v1" });
+                
             });
             services.AddLogging();
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+           
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -44,10 +48,13 @@ namespace Calculator.WebApi
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
+
+
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix="";
+                c.SwaggerEndpoint(url: Configuration["AppSettings:VirtualDirectory"]+ "/swagger/swagger/v1/swagger.json", name: "My API V1");
             });
 
             app.UseMvc();
